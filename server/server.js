5b71aecede7773cf8e78bf392eff2aa4660e0cc3
Arash <arashcyruss@gmail.com>
2017-11-24 13:27:47 -0700

    Add generateMessage utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var http = require('http');
 var socketIO = require('socket.io');
+var {generateMessage} = require('./utils/message')
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -15,16 +16,10 @@ var io = socketIO(server);
 //gets called when a connection between server and client is created
 io.on('connection', function(socket){
     console.log('new user connected');
-    socket.emit('newMessage', {
-      from: 'admin',
-      message: 'welcome',
-      createdAt: new Date().getTime()
-    })
-    socket.broadcast.emit('newMessage', {
-      from: 'admin',
-      message: 'New user joined',
-      createdAt: new Date().getTime()
-    })
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the app'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
     socket.on('disconnect', () => {
       console.log("user disconnected from server");
     });
@@ -33,11 +28,7 @@ io.on('connection', function(socket){
     // listens for a createMessage event from client
     socket.on('createMessage', (message) => {
       console.log('create message', message);
-      io.emit('newMessage', {   //emits an event for all users
-        from: message.from,
-        text: message.text,
-        createdAt: new Date().getTime
-      })
+      io.emit('newMessage', generateMessage(message.from, message.text));
   })
 
 
